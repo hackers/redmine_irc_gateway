7f8a7ee101f2ceb6194fb16f5c679acf23752343
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-07-19 18:27:43 +0900

    fix bug. change default option, :localhost to nil

--- lib/redmine_irc_gateway/server.rb
+++ lib/redmine_irc_gateway/server.rb
@@ -6,10 +6,10 @@ module RedmineIRCGateway
     attr_accessor :opts
 
     def initialize
-      opts = Slop.parse :help => true do
+      @opts = Slop.parse :help => true do
         banner "Usage: #{$0} [options]"
         on :p, :port,    'Port number to listen',             true,  :as => :integer, :default => 16700
-        on :h, :host,    'Host name or IP address to listen', true,  :as => :string,  :default => :localhost
+        on :h, :host,    'Host name or IP address to listen', true,  :as => :string,  :default => nil
         on :l, :log,     'log file',                          true,  :as => :string,  :default => nil
         on :d, :debug,   'Enable debug mode',                 false, :as => :boolean, :default => false
         on :v, :version, 'Print the version' do
@@ -18,18 +18,17 @@ module RedmineIRCGateway
         end
       end.to_hash
 
-      opts[:logger] = Logger.new STDOUT
-      opts[:logger].level = opts[:debug] ? Logger::DEBUG : Logger::INFO
-
-      @opts = opts
+      @opts[:logger] = Logger.new STDOUT
+      @opts[:logger].level = @opts[:debug] ? Logger::DEBUG : Logger::INFO
     end
 
     def self.run!
       self.new.run!
     end
 
-    def run!
+    def start
       Net::IRC::Server.new(@opts[:host], @opts[:port], RedmineIRCGateway::Session, @opts).start
     end
+    alias :run! :start
   end
 end
