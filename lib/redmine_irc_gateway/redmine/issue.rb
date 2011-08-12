686758456eeead2ffe2ce58e70a755ae669d9098
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-12 20:03:17 +0900

    add methods. assigned_me, watched

--- lib/redmine_irc_gateway/redmine/issue.rb
+++ lib/redmine_irc_gateway/redmine/issue.rb
@@ -1,5 +1,13 @@
 module RedmineIRCGateway
   module Redmine
-    class Issue < API; end
+    class Issue < API
+      def self.assigned_me
+        all({ :params => { :assigned_to_id => :me } })
+      end
+
+      def self.watched
+        all({ :params => { :watcher_id => :me } })
+      end
+    end
   end
 end
