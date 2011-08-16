f8b6beeb1383d97a755ca7a881869698b18836b4
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-16 21:47:20 +0900

    add logger

--- lib/redmine_irc_gateway/redmine/api.rb
+++ lib/redmine_irc_gateway/redmine/api.rb
@@ -8,10 +8,14 @@ module RedmineIRCGateway
       self.user = config.user
       self.password = config.password
       self.proxy = ENV['http_proxy'] if ENV['http_proxy']
+      self.logger = Logger.new STDOUT
 
       class << self
         def find(*args)
-          super rescue []
+          super
+        rescue => e
+          self.logger.error e.to_s
+          nil
         end
 
         def all(params = nil)
