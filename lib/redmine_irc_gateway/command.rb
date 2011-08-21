79ecf930143f594efd5eefb6156b03c128d441fc
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-21 19:41:15 +0900

    add get watched issue command

--- lib/redmine_irc_gateway/command.rb
+++ lib/redmine_irc_gateway/command.rb
@@ -5,7 +5,15 @@ module RedmineIRCGateway
     def list
       issues = []
       Redmine::Issue.assigned_me.each do |i|
-        issues << "[#{i.project.name}] ##{i.id} #{i.subject}"
+        issues << "[#{i.project.name}] ##{i.id} #{i.subject} #{i.uri}"
+      end
+      issues
+    end
+
+    def watch
+      issues = []
+      Redmine::Issue.watched.each do |i|
+        issues << "[#{i.project.name}] ##{i.id} #{i.subject} #{i.uri}"
       end
       issues
     end
