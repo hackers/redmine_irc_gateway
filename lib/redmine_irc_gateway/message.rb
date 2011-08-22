c90f12bb546654274ab44d09ce497dcc2bb3e17f
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-22 11:30:04 +0900

    extend Net::IRC:Message

--- lib/redmine_irc_gateway/message.rb
+++ lib/redmine_irc_gateway/message.rb
@@ -1,25 +1,10 @@
 module RedmineIRCGateway
-  class Message
+  class Message < Net::IRC::Message
+    attr_accessor :channel, :content
 
-    class << self
-      def parse m
-        self.new m
-      end
-    end
-
-    attr_accessor :command, :channel, :content
-
-    def initialize m
-      @command = m.command
-      @channel, @content = m.params
-    end
-
-    def to_s
-      @content
-    end
-
-    def to_sym
-      @content.to_sym
+    def initialize(*args)
+      super
+      @channel, @content = @params
     end
 
     # TODO
@@ -37,5 +22,35 @@ module RedmineIRCGateway
     def topic m
       @command = TOPIC
     end
+
+  end
+end
+
+class Net::IRC::Message
+  #  Monkey patch Net::IRC::Message.parse
+  #  call RedmineIRCGateway::Message.new instead of Net::IRC::Message.new
+  def self.parse(str)
+    _, prefix, command, *rest = *PATTERN::MESSAGE_PATTERN.match(str)
+    raise InvalidMessage, "Invalid message: #{str.dump}" unless _
+
+    case
+    when rest[0] && !rest[0].empty?
+      middle, trailer, = *rest
+    when rest[2] && !rest[2].empty?
+      middle, trailer, = *rest[2, 2]
+    when rest[1]
+      params  = []
+      trailer = rest[1]
+    when rest[3]
+      params  = []
+      trailer = rest[3]
+    else
+      params  = []
+    end
+
+    params ||= middle.split(/ /)[1..-1]
+    params << trailer if trailer
+
+    RedmineIRCGateway::Message.new(prefix, command, params)
   end
 end
