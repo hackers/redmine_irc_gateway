b450c5581585c193938e1dfd107a33963d4fe213
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-27 11:44:11 +0900

    add online_users test

--- test/test_redmine.rb
+++ test/test_redmine.rb
@@ -4,16 +4,23 @@ module RedmineIRCGateway
   module Redmine
     class RedmineTest < ActiveSupport::TestCase
 
+      test 'Load order module' do
+        assert_kind_of Module, Redmine
+      end
+
+      test 'Get online users' do
+        me = User.current
+        me.projects.each do |p|
+          assert_kind_of Array, Redmine.online_users(p.id)
+        end
+      end
+
       test 'Check time entry class' do
         assert_equal 'RedmineIRCGateway::Redmine::TimeEntry', TimeEntry.to_s
       end
 
       test 'Check version class' do
         assert_equal 'RedmineIRCGateway::Redmine::Version', Version.to_s
-      end
-
-      test 'Load order module' do
-        assert_kind_of Module, Redmine
       end
 
       test 'Call list order' do
