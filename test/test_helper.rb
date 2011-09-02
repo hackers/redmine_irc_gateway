3a842a845e5c3eeef43552415d521f6b92ae6cb6
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-09-02 19:16:13 +0900

    support v192 refs #13

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,3 @@
-$: << File.expand_path('../../lib', __FILE__)
-$: << File.expand_path('../../test', __FILE__)
-
 require 'test/unit'
 require 'redmine_irc_gateway'
 
--- test/test_version.rb
+++ test/test_version.rb
@@ -1,4 +1,4 @@
-require 'test/test_helper'
+require_relative 'test_helper'
 
 module RedmineIRCGateway
   class VersionTest < ActiveSupport::TestCase
