67953ed9b2adae906940b008ef923026aaaeb37b
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-09-05 19:07:58 +0900

    extend OpenStruct, and add comment

--- lib/redmine_irc_gateway/message.rb
+++ lib/redmine_irc_gateway/message.rb
@@ -1,17 +1,19 @@
 module RedmineIRCGateway
-  class Message
-
-    attr_accessor :channel, :content, :speaker, :project_id
-
-    def initialize args
-      @channel    = args[:channel]
-      @project_id = args[:project_id]
-      @speaker    = args[:speaker]
-      @content    = args[:content]
-    end
+  # This class is message object for irc and Redmine issue.
+  #
+  # == An example
+  #
+  #     m = Message.new({
+  #          :channel    => '#channel',
+  #          :project_id => 'Redmine project id',
+  #          :content    => 'message body',
+  #          :speaker    => 'IRC nick or Redmine user name'
+  #        })
+  #     puts m.channel # channel name
+  class Message < OpenStruct
 
     def instruction
-      @content.downcase.to_sym
+      content.downcase.to_sym
     end
 
   end
